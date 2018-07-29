81b0d02e941e2f60ebb626c1ed6f3ce5dc8526e8
Bohdan Konorin <KonorinDeveloper@gmail.com>
2018-07-30 01:59:46 +0300

    Added backend

---
+++ Airport_Backend/Airport_REST_API.Shared/DTO/TicketDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Airport_REST_API.Shared.DTO
+{
+    public class TicketDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [Range(1.0, 10000.0, ErrorMessage = "Price must be between $1 and $10000")]
+        public double Price { get; set; }
+
+        [Required]
+        [MinLength(4,ErrorMessage = "Number must be more than 4 letters")]
+        public string Number { get; set; }
+    }
+}
